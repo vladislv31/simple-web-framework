b7c9b9a0dfae0d4be073c25e65db429b9d654a1c
vladislv31 <vladislv.k31@gmail.com>
2020-08-17 01:10:44 +0300

    routing

--- app.py
+++ app.py
@@ -3,13 +3,38 @@ from webob import Request, Response
 
 
 class App:
+    def __init__(self):
+        self.routes = {}
+
+    def route(self, path):
+        def wrapper(handler):
+            self.routes[path] = handler
+            return handler
+
+        return wrapper
+
     def __call__(self, environ, start_response):
         request = Request(environ)
 
-        response = Response()
-        response.text = 'Hello'
+        response = self.handle_request(request)
 
         return response(environ, start_response)
+
+    def handle_request(self, request):
+        current_path = request.path
+        response = Response()
+
+        for path, handler in self.routes.items():
+            if path == current_path:
+                handler(request, response)
+                return response
+
+        self.default_response(response)
+        return response
+
+    def default_response(self, response):
+        response.status = 404
+        response.text = '404. Not found...'
 
     def serve(self, host='localhost', port=8000):
         try:
--- main.py
+++ main.py
@@ -2,4 +2,20 @@ from app import App
 
 
 app = App()
+
+
+@app.route('/')
+def index(req, resp):
+    resp.text = 'Index page'
+
+@app.route('/about')
+def index(req, resp):
+    resp.text = 'About page'
+
+@app.route('/google')
+def index(req, resp):
+    resp.status = 307
+    resp.location = 'http://google.com'
+
+
 app.serve()
